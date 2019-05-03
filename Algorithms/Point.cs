bdfe2bbac9590281f81fabc03da72e8e86345c6f
unknown <dansru@gmail.com>
2019-05-04 01:45:45 +0300

    Initial commit

---
+++ Algorithms/Point.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Point : IComparable<Point>, IComparer<Point>
+    {
+        public int x;
+        public int y;
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int Compare(Point p2, Point p3)
+        {
+            double slopeP2 = this.SlopeTo(p2);
+            double slopeP3 = this.SlopeTo(p3);
+            return 
+        }
+
+        public int CompareTo(Point p2)
+        {
+            if (this.y != p2.y)
+                return this.y.CompareTo(p2.y);
+            else
+                return this.x.CompareTo(p2.x);
+        }
+
+        public double SlopeTo(Point p2)
+        {
+            if ((p2.x == this.x) && (p2.y == this.y))
+            {
+                return double.NegativeInfinity;
+            }
+            else if ((p2.x - this.x) == 0)
+            {
+                return double.PositiveInfinity;
+            }
+            else if ((p2.y - this.y) == 0)
+            {
+                return 0;
+            }
+            else return (p2.y - this.y) / (p2.x - this.x);
+
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the array");
+            int el = 0;
+            List<int> arr = new List<int>();
+            while (el != -1)
+            {
+                el = Convert.ToInt32(Console.ReadLine());
+                arr.Add(el);
+
+            }
+            int[] arrS = arr.ToArray();
+            Merge.sort(arrS);
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class Merge
+    {
+        static int[] a;
+
+        public static void merge(int[] a, int[] aux, int lo, int mid, int hi)
+        {
+            for(int k = lo; k <= hi; k++)
+            {
+                aux[k] = a[k];
+            }
+
+            int i = lo, j = mid + 1;
+            for(int k = lo; k <= hi; k++)
+            {
+                if (i > mid) a[k] = aux[j++];
+                else if (j > hi) a[k] = aux[i++];
+                else if (aux[j] < aux[i]) a[k] = aux[j++];
+                else a[k] = aux[i++];
+
+            }
+        }
+
+        public static void sort(int[] a)
+        {
+            var aux = new int[a.Length];
+            sort(a, aux, 0, a.Length - 1);
+        }
+
+        public static void sort(int[] a, int[] aux, int lo, int hi)
+        {
+            if (hi <= lo) return;
+            int mid = (lo + hi) / 2;
+            sort(a, aux, lo, mid);
+            sort(a, aux, mid + 1, hi);
+            merge(a, aux, lo, mid, hi);
+
+
+        }
+    }
+}
